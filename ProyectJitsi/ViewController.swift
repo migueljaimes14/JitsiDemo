9375e302951ef0fc8478b7d08942a5ed53fe28d4
Miguel Jaimes Albornoz <mja@thinkofdigital.com>
2020-02-17 09:59:09 +0100

    Initial Commit

---
+++ ProyectJitsi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectJitsi
+//
+//  Created by DEV-IOS on 17/02/2020.
+//  Copyright © 2020 DEV-IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
